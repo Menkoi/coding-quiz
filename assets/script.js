5db2a3e585ee78ade1dc7ad1471860aeb2411b81
celaeon <celaeon@gmail.com>
2021-01-04 17:18:24 -0500

    clicking start btn brings to quiz rules

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
+// elements 
+var startBtn = document.querySelector(".start-btn button");
+var infoBox = document.querySelector(".info-box");
+var exitBtn = document.querySelector(".buttons .quit");
+var continueBtn = document.querySelector(".buttons .restart")
+
+// start quiz button clicked
+startBtn.onclick = ()=>{
+    infoBox.classList.add("activeInfo");
+}
+
+// start quiz button clicked
+exitBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo");
+}
+
+
 
