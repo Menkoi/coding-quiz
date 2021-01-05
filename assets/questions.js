2f7592f8a3b201d7eecb181dded47a8d78c7b056
celaeon <celaeon@gmail.com>
2021-01-05 18:48:18 -0500

    added question and answer through JS

---
+++ assets/questions.js
@@ -0,0 +1,84 @@
+//questions array, pass number, question, options, and questions
+
+let questions = [
+    {
+        numb: 1,
+        question: "Лорем ипсум долор сит амет?",
+        answer: "Hyper aaaaaaaa",
+        options: [
+            "Hyper aaaaaaaa",
+            "yyyyyyyyyyyyyy",
+            "ffffffffffffffff",
+            "ggggggggggggggggg"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Лорем ипсум долор сит амет?",
+        answer: "Тале инани но сит",
+        options: [
+            "партиендо глориатур не нец",
+            "Тале инани но сит",
+            "Не ехплицари еурипидис нец",
+            "Еи сеа игнота сцрипторем"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "写ミシ気難際るばよ戦能リノモ補大サ応共すラちく朝92?",
+        answer: "段頼な地収ワセカス省理果場つをがだ",
+        options: [
+            "世んゆれッ目枚どうょル控",
+            "段頼な地収ワセカス省理果場つをがだ",
+            "直ヨマ宇前済がっへぎ上8示",
+            "祭シヲト能成共ヱユカ金道再ドぐ"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "국회는 국가의 예산안을 심의?",
+        answer: "형사피고인은 상당한 이유가 없는 한",
+        options: [
+            "헌법재판소의 조직과 운영 기타 필요한",
+            "모든 국민은 주거의 자유를 침해받지 아니한다",
+            "동의를 얻어 대통령이 임명하고",
+            "형사피고인은 상당한 이유가 없는 한"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Negi movi proto ien ej?",
+        answer: " Re inter artefarita multiplikite nia",
+        options: [
+            "suba koruso hodiaŭa oz ene",
+            " Re inter artefarita multiplikite nia",
+            "Poa ng modo kilo rolvorteto",
+            "Nek reciproke centimetro ve, jes fi kiomas "
+        ]
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,40 @@
 // elements 
 var startBtn = document.querySelector(".start-btn button");
 var infoBox = document.querySelector(".info-box");
-var exitBtn = document.querySelector(".buttons .quit");
-var continueBtn = document.querySelector(".buttons .restart")
+var exitBtn = infoBox.querySelector(".buttons .quit");
+var continueBtn = infoBox.querySelector(".buttons .restart");
+var quizBox = document.querySelector(".quiz-box");
 
 // start quiz button clicked
 startBtn.onclick = ()=>{
     infoBox.classList.add("activeInfo");
 }
 
-// start quiz button clicked
+// exit quiz button clicked
 exitBtn.onclick = ()=>{
     infoBox.classList.remove("activeInfo");
+}
+
+// continue quiz button clicked
+continueBtn.onclick = ()=>{
+    infoBox.classList.remove("activeInfo");
+    quizBox.classList.add("activeQuiz"); // show quiz box 
+    showQuestions(4);
+}
+
+let que_count = 0
+
+// question and options from array
+function showQuestions(index) {
+    var que_text = document.querySelector(".que-text");
+    var option_list = document.querySelector(".option-list");
+    let que_tag = '<span>' + questions[index].question + '</span>';
+    let option_tag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
 }
 
 
